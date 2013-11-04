8531860500a51dfae97b9b2b274cb8eea3cdbade
Richard Szalay <richard.szalay@profero.com>
2013-11-05 09:00:26 +1100

    Added Pester test for Add-HostEntry

---
+++ RichardSzalay.Hosts.Powershell.Tests/Get-HostEntry.Tests.ps1
@@ -0,0 +1,47 @@
+#$hostsFile = $env:temp + "\pshosts_hosts"
+
+# TODO: Should this occur outside the tests so that the tests can be run against any build/instance of the module?
+Import-Module "$PSScriptRoot\..\RichardSzalay.Hosts.Powershell\bin\Debug\PsHosts.psd1" -Prefix Test -Force
+
+$hostsFile = [System.IO.Path]::GetTempFileName()
+Describe "Get-HostEntry" {
+    AfterEach {
+        Remove-Item $hostsFile
+    }
+
+    Context "Without arguments when there are entries" {
+        "127.0.0.1 hostname`n127.0.0.1 hostname2" > $hostsFile
+        $results = Get-TestHostEntry -HostsPath $hostsFile
+
+        It "returns all entries" {
+            $results.length | Should Be 2
+        }
+    }
+
+    Context "With named host when there is a match" {
+        "127.0.0.1 hostname`n127.0.0.1 hostname2" > $hostsFile
+        $results = Get-TestHostEntry hostname -HostsPath $hostsFile
+
+        It "returns matching entry" {
+            $results.length | Should Be 1
+        }
+    }
+
+    Context "With named host when there are duplicate matches" {
+        "127.0.0.1 hostname`n127.0.0.1 hostname" > $hostsFile
+        $results = Get-TestHostEntry hostname -HostsPath $hostsFile
+
+        It "returns matching entries" {
+            $results.length | Should Be 2
+        }
+    }
+
+    Context "With wildcard name when there are entries" {
+        "127.0.0.1 hostname`n127.0.0.1 hostname2" > $hostsFile
+        $results = Get-TestHostEntry hostname* -HostsPath $hostsFile
+
+        It "returns matching entries" {
+            $results.length | Should Be 2
+        }
+    }
+}
