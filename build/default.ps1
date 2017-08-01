376de26bf9ac59e19ee455b334f0e272dbdac033
Richard Szalay <richard.szalay@loweprofero.com>
2017-08-01 14:04:31 +1000

    Build now uploads pester results to Appveyor, when running in that environment

--- build/default.ps1
+++ build/default.ps1
@@ -25,7 +25,14 @@ task TestCmdlets -depends Compile {
       param($modulePath, $pattern)
 
       Import-Module $modulePath -Force      
-      Invoke-Pester -Script @{ Path = $pattern }
+      Invoke-Pester -Script @{ Path = $pattern } -OutputFormat NUnitXml -OutputFile .\ps-results.xml
+
+      if ($env:APPVEYOR_JOB_ID)
+      {
+        $wc = New-Object 'System.Net.WebClient'
+        $wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\ps-results.xml))
+      }
+
   } -args "$PSScriptRoot\..\RichardSzalay.Hosts.Powershell\bin\$configuration\PsHosts\PsHosts.psd1",$testFilePattern
 }
 
