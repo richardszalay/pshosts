189c1a54b2491ab2a1b6d166c646042142d47754
Richard Szalay <richard.szalay@loweprofero.com>
2017-08-01 13:36:47 +1000

    Build now executes mspec tests using appveyor, when available

--- build/default.ps1
+++ build/default.ps1
@@ -9,8 +9,17 @@ properties {
   $nugetDir = "$toolsDir\nuget.exe"
   $solutionPath = Join-Path $PSScriptRoot "..\RichardSzalay.Hosts.sln"
   $testFilePattern = "$PSScriptRoot\..\RichardSzalay.Hosts.Powershell.Tests\*"
-  $mspecCliPath = "$PSScriptRoot\..\packages\Machine.Specifications.0.5.16\tools\mspec-clr4.exe"
   $libPath = "$PSScriptRoot\..\RichardSzalay.Hosts.Tests\bin\$configuration\RichardSzalay.Hosts.Tests.dll"
+
+  if ($env:APPVEYOR)
+  {
+    # Appveyor adds a test result friendly version of mspec to the path
+    $mspecCliPath = "mspec"
+  }
+  else
+  {
+    $mspecCliPath = "$PSScriptRoot\..\packages\Machine.Specifications.0.5.16\tools\mspec-clr4.exe"
+  }
 }
 
 task default -depends Test
